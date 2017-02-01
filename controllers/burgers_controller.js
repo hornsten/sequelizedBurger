82fda7e27da3c315cc59faacb983cf90a4cd1bb2
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-02-01 10:43:45 -0600

    Basic version solved

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,34 +1,18 @@
 var express = require('express');
 var router = express.Router();
-var methodOverride = require('method-override');
-var models = require('../models');
-var Burger = models.Burger;
-var Customer = models.Customer;
-var Sequelize = require('sequelize');
-var bodyParser = require('body-parser');
-
+var db = require('../models');
 
 //Grabs all the data from the burgers table...all of the burgers! These are sorted later with Handlebars #if and #unless statements
 router.get("/", function(req, res) {
 
-    Burger.findAll({
-        // include: [{
-        //     model: Customer
-        // }]
-    }).then(function(data) {
+    db.Burger.findAll({}).then(function(data) {
         res.render("index", { burgers: data });
     })
 });
 
-// router.get('/', function(req, res) {
-//     Customer.findAll({}).then(function(data) {
-//         res.render("index", { customers: data });
-//     })
-// })
-
 //User can enter a new burger into the system. Default values are already set for id, devoured=false, createdAt and updatedAt
 router.post("/", function(req, res) {
-    Burger.create({
+    db.Burger.create({
         burger_name: req.body.burger_name
     }).then(function() {
         res.redirect("/");
@@ -36,7 +20,7 @@ router.post("/", function(req, res) {
 });
 
 router.post('/', function(req, res) {
-    Customer.create({
+    db.Customer.create({
         customer_name: req.body.customer_name
     }).then(function() {
         res.redirect('/');
@@ -45,7 +29,7 @@ router.post('/', function(req, res) {
 
 //Changes status of burger from devoured=false to devoured=true
 router.put("/:id", function(req, res) {
-    Burger.update({
+    db.Burger.update({
         devoured: true
     }, {
         where: {
--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,21 @@
 var Sequelize = require('sequelize');
 
-var Customer = require('./Customer');
-
 module.exports = function(sequelize, DataTypes) {
 
     var Burger = sequelize.define("Burger", {
 
-        burger_name: DataTypes.STRING,
-        devoured: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false }
+        burger_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        devoured: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
     });
     return Burger;
     Burger.sync();
