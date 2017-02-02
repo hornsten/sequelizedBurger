950d8cffb41a7b49db0aad6e0c7b5c14ed88cef2
Jessica Hornsten <hornsten@users.noreply.github.com>
2017-02-01 18:37:27 -0600

    Troubleshooting

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,28 @@
-In this file is everything needed to establish connection with MySQL db
-var mysql = require("mysql");
+// In this file is everything needed to establish connection with MySQL db
+// var mysql = require("mysql");
 
-var connection;
+// var connection;
 
-if (process.env.NODE_URL) {
-    connection = mysql.createConnection(process.env.NODE_URL);
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "pass",
-        database: "burgers_database"
-    });
-};
+// if (process.env.NODE_URL) {
+//     connection = mysql.createConnection(process.env.NODE_URL);
+// } else {
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         user: "root",
+//         password: "pass",
+//         database: "burgers_database"
+//     });
+// };
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
 
-    console.log("connected as id " + connection.threadId);
+//     console.log("connected as id " + connection.threadId);
 
-});
+// });
 
-//exports this file content so other files can access it
-module.exports = connection;
+// //exports this file content so other files can access it
+// module.exports = connection;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
--- A few entries to populate the new table created using schema.sql
-INSERT INTO burgers (burger_name,devoured) VALUES ('Cheeseburger',false);
-INSERT INTO burgers (burger_name,devoured) VALUES ('Turkey Burger',false);
-INSERT INTO burgers (burger_name,devoured) VALUES ('Falafel Burger',false);
+-- -- A few entries to populate the new table created using schema.sql
+-- INSERT INTO burgers (burger_name,devoured) VALUES ('Cheeseburger',false);
+-- INSERT INTO burgers (burger_name,devoured) VALUES ('Turkey Burger',false);
+-- INSERT INTO burgers (burger_name,devoured) VALUES ('Falafel Burger',false);
